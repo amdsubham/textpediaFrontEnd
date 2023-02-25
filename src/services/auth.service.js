45a18fc03d93651264ea72dc1d7653132d58e38c
Subham <Subham@gmail.com>
2023-02-25 16:04:15 +0530

    share

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8085/api/auth/";
+const API_URL = "http://31.220.21.195:8085/api/auth/";
 
 class AuthService {
   login(username, password) {
